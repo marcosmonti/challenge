f5cfe058e2a72d550f0b8e055889d1bfad6e6a9a
Marcos Monti <marcosmonti@gmail.com>
2018-03-06 15:39:30 -0300

    static files

--- challenge/settings.py
+++ challenge/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 
 # DRF Settings
 REST_FRAMEWORK = {
